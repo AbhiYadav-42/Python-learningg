2902f13fc21babb055437e1f662a5b2715c49f71
AbhiYadav-42 <1alchemax42@gmail.com>
2025-07-14 09:46:50 +0530

    Practice set of Variables

---
+++ practice set/practice-2.py
@@ -0,0 +1,46 @@
+#write a program to add two numbers
+"""Q-1
+A= int(input("Enter the first number:"))
+B= int(input("Enter the second number"))
+
+c=A+B
+print("sum of A and B is :", A+B)
+"""
+
+# Write a progrom to find the remainder when dividing with z
+"""Q-2
+A= int(input("enter the number"))
+z=int(input("enter the z:"))
+
+remainder=A/z
+
+print("The remainder we got = ", remainder)
+"""
+
+#check the type of varibale assignes using input() function 
+"""Q-3
+A= input("Enter:")
+print("The type of Variable:",type(A))
+"""
+
+# Use comparison operator to find out whe ther a given variable A is greater than b or not , Take A= 34, B= 80
+"""Q-4
+a=int(input("Enter the number for A : "))
+b= int(input("Enter the number for B : "))
+print("A is greater than B or not -->>", a>b) 
+"""
+
+# Write a program to find an average of two numbers enterd by the user.
+"""Q-5
+A=int(input("Enter the first number: "))
+B=int(input("Enter the second number: "))
+
+print("The average of first and second number: ", (A+B)/2)
+"""
+
+# Write a python program to calculate the square of number enterd by a user 
+"""Q-6
+A=int(input("Enter the First number:  "))
+print("Sqaure of number : ",A*A)
+"""
+
