5117e7a33868aa90fa492da50c7a04539a07deae
Abhi Yadav <1alchemax42@gmail.com>
2025-07-17 23:52:21 +0530

    Did the pratice set-4 (list and tuples)

---
+++ practice set/Practice-4.py
@@ -0,0 +1,67 @@
+#Write a program to create a list to store a 7 fruits given by the user.
+"""Q-1
+Fruits=[]
+
+f=input("Enter the 1 names of the fruits: ")
+Fruits.insert(0,f)
+
+f1=input("Enter the 2 names of the fruits: ")
+Fruits.insert(1,f1)
+
+f2=input("Enter the 3 names of the fruits: ")
+Fruits.insert(2,f2)
+
+f3=input("Enter the 4 names of the fruits: ")
+Fruits.insert(3,f3)
+
+f4=input("Enter the 5 names of the fruits: ")
+Fruits.insert(4,f4)
+
+print(Fruits)
+"""
+
+#
+"""Q-2
+Student=[]
+
+f=input("Enter the Marks of the student: ")
+Student.insert(0,f)
+
+f1=input("Enter the Marks of the student: ")
+Student.insert(1,f1)
+
+f2=input("Enter the Marks of the student :")
+Student.insert(2,f2)
+
+f3=input("Enter the Marks of the student: ")
+Student.insert(3,f3)
+
+f4=input("Enter the Marks of the student :")
+Student.insert(4,f4)
+
+
+Student.sort()
+print(Student)
+"""
+
+
+# Show that Tuple is unmutable
+"""Q-3
+a=(1,2,"Harry")
+
+a[3]="Larry"
+print(a)  
+#OUTPUT: 'tuple' object does not support item assignment
+"""
+# Write a program sum the 4 elements of the List 
+"""Q-4
+a=[1,2,4,5,6,7,8,]
+print("The sum of the list:  ",sum(a))
+"""
+
+#Count Zeros in the Tuple
+
+a=(0,1,2,0,0,2,0,1,2,0)
+b=a.count(0)
+print("Zeros:",b)
+
