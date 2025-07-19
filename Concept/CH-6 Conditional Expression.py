54f80fe62360a53eb6f26b17dec46f750b7ffc5c
Abhi Yadav <1alchemax42@gmail.com>
2025-07-19 21:41:23 +0530

    completed  if and else

---
+++ Concept/CH-6 Conditional Expression.py
@@ -0,0 +1,26 @@
+# if else and elif are the multiway  descision taken by our program due to certain conditions in our code 
+
+#This is the best example of Conditional expression (if n else) 
+
+#         Club Entry:
+Bouncer=print("What's your Age:")
+Age=int(input("My age is : "))
+
+if(Age>=21):
+  print("You are good to go sir🙇")
+  print("Have a marvelous night sir 🙇‍♂️")
+
+elif(Age<=0):
+  print("B***H, This ain't funny 💢🤬 ")
+
+elif(Age>=60):
+  print("This way sir🙇")
+
+else:
+  print("What you think, Moron💢 ")
+  print("GET the F**k out of here🤬 ")
+
+
+# There can be as many if and elif statements as you want, but only if statements can stand alone. elif and else must always follow an if.
+
+
