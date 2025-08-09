d3372c7bc8b19c92d0d885826e03b049a8982186
Abhi Yadav <1alchemax42@gmail.com>
2025-08-09 23:27:47 +0530

    Q-2

---
+++ practice set/questionns/q-2.py
@@ -0,0 +1,10 @@
+# write a program to greet all the person names  stored in a list 'l' and which starts with S.
+"""Q-2"""
+l=["harry", "Soham","Sachin", "Rahul", "ajay" ]
+
+for i in l:
+  if(i.startswith("S")):
+   print(f"Good morning  :{i}")
+  
+  else:
+    print("......")
