ebaccffaf14dcb04804c681e9e4e54b3fbe2e83e
Abhi Yadav <1alchemax42@gmail.com>
2025-08-10 18:42:10 +0530

    4 pillars of OOPs

---
+++ OOPs/4_pillars.py
@@ -0,0 +1,14 @@
+# There are 4 pillars of OOPs
+
+"""
+1. Abstraction -> Hiding the implementation details of a class and only showing the essential features to the user
+"""
+"""
+2. Encapsulation -> Wrapping data and functions into a single unit(object).
+"""
+"""
+3. Inheritance -> 
+"""
+"""
+4. Polymorphis ->
+"""
