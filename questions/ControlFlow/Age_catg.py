ef8614548fadea0f76b3d6619651317c201963c1
Abhi Yadav <1alchemax42@gmail.com>
2025-10-09 19:50:57 +0530

    Age Catgorized

---
+++ questions/ControlFlow/Age_catg.py
@@ -0,0 +1,6 @@
+Age = int(input("Emter your Age: "))
+
+if(Age>=60): print("Senior citzen!!")
+elif(Age>=20 and Age<=59): print("Adult!!")
+elif(Age>=13 and Age<=19): print ("Teenager!!")
+else: print("Child!!")
