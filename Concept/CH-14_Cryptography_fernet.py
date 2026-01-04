03116874940eef0d25b4fb28152193cbcb8593c2
Abhi Yadav <1alchemax42@gmail.com>
2026-01-04 17:39:41 +0530

    learning: cryptography basics with Fernet

--- Concept/CH-14_Cryptography_fernet.py
+++ Concept/CH-14_Cryptography_fernet.py
@@ -27,3 +27,54 @@ Decrypt_conversion = k.decrypt(conversion)
 
 # display the decrypted text
 print(Decrypt_conversion)
+
+
+
+
+
+
+""" Using Passwords with Fernet """
+
+import os
+from cryptography.fernet import Fernet
+from base64 import urlsafe_b64encode
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.backends import default_backend
+
+password = b'84260'
+
+# Adding salting with randomness of OS 
+salt = os.urandom(16)
+print(salt)
+
+# Key derivation function -  cryptographic hash function 
+# Password Based Key Derivation Function 2 hash-based message authentication 
+kdf = PBKDF2HMAC(
+  algorithm=hashes.SHA256() ,
+  length=32,
+  salt=salt,
+  iterations=100000,
+  backend=default_backend()
+)
+
+# Derive key 
+key = kdf.derive(password)
+
+
+# verify the key - need to make another kdf (i am not gonna do.) 
+# kdf.verify(password,key)
+
+#Make key-fernet compatible
+Fernet_key = urlsafe_b64encode(key)
+
+#create a fernet object
+f = Fernet(Fernet_key)
+
+#Encrypt 
+add_password = f.encrypt(b'abhii@134')
+print(add_password)
+
+#Decrypt
+de = f.decrypt(add_password).decode('utf-8')
+print(de)
